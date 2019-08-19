8422622aa73a8d801a6a08f4191a538126859f64
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-19 22:28:45 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all if Rails.env.development?
-Companies.destroy_all if Rails.env.development?
 Company.destroy_all if Rails.env.development?
 Group.destroy_all if Rails.env.development?
+Companies_Pointer.destroy_all if Rails.env.development?
 
 puts "creating sample..."
 
+User.create(name: "Alexander", email: "test@gmail.com", password: "123123")
+User.create(name: "Helen", email: "troy@gmail.com", password: "123123")
 
+#bigger companies
+Company.create(ticker: "AAPL")
+Company.create(ticker: "GOOG")
+Company.create(ticker: "AMZN")
+Company.create(ticker: "TSLA")
+Company.create(ticker: "UBER")
+Company.create(ticker: "MSFT")
+Company.create(ticker: "NFLX")
 
+#smaller companies
+Company.create(ticker: "LL")
+Company.create(ticker: "SWI")
+Company.create(ticker: "BWLD")
 
+#indexes
+Company.create(ticker: ".DJI")
+Company.create(ticker: ".INX")
+Company.create(ticker: "NYA")
+
+4.times do
+  Group.create(user: rand.(1..2))
+end
+
+13.times do
+  Companies_Pointer.create(group_id: rand(1..4), company_id: rand(1..13))
+end
 
 puts. "created sample"
