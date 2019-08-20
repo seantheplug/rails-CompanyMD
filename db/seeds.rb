4bd7258968e841d50b4da7d3a8ec394c64a9a856
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-20 09:27:48 +0900

    added ! to all creates for exception errors to be raised if fail

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,34 +13,34 @@ Companies_Pointer.destroy_all if Rails.env.development?
 
 puts "creating sample..."
 
-User.create(name: "Alexander", email: "test@gmail.com", password: "123123")
-User.create(name: "Helen", email: "troy@gmail.com", password: "123123")
+User.create!(name: "Alexander", email: "test@gmail.com", password: "123123")
+User.create!(name: "Helen", email: "troy@gmail.com", password: "123123")
 
 #bigger companies
-Company.create(ticker: "AAPL")
-Company.create(ticker: "GOOG")
-Company.create(ticker: "AMZN")
-Company.create(ticker: "TSLA")
-Company.create(ticker: "UBER")
-Company.create(ticker: "MSFT")
-Company.create(ticker: "NFLX")
+Company.create!(ticker: "AAPL")
+Company.create!(ticker: "GOOG")
+Company.create!(ticker: "AMZN")
+Company.create!(ticker: "TSLA")
+Company.create!(ticker: "UBER")
+Company.create!(ticker: "MSFT")
+Company.create!(ticker: "NFLX")
 
 #smaller companies
-Company.create(ticker: "LL")
-Company.create(ticker: "SWI")
-Company.create(ticker: "BWLD")
+Company.create!(ticker: "LL")
+Company.create!(ticker: "SWI")
+Company.create!(ticker: "BWLD")
 
 #indexes
-Company.create(ticker: ".DJI")
-Company.create(ticker: ".INX")
-Company.create(ticker: "NYA")
+Company.create!(ticker: ".DJI")
+Company.create!(ticker: ".INX")
+Company.create!(ticker: "NYA")
 
 4.times do
-  Group.create(user: rand.(1..2))
+  Group.create!(user: rand.(1..2))
 end
 
 13.times do
-  Companies_Pointer.create(group_id: rand(1..4), company_id: rand(1..13))
+  Companies_Pointer.create!(group_id: rand(1..4), company_id: rand(1..13))
 end
 
 puts. "created sample"
