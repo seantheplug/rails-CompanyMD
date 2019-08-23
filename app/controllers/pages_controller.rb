82c6d5d0daa882e0a019fbcc0dfd537b1a9d1757
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-23 15:04:00 +0900

    removed new search functions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,24 +14,5 @@ class PagesController < ApplicationController
     if params[:query].present?
       @search_array = search_company(params[:query])
     end
-
-    @search_array.each do |company|
-      if Company.exists?(ticker: company["1. symbol"])
-        next
-      else
-        Company.create(ticker: company["1. symbol"], name: company["2. name"])
-      end
-      raise
-    end
-
-    # @company = Company.new(ticker: "msft")
-    # if @company.save
-    #   redirect_to company_path(@company)
-    # else
-    #   render :search
-    # end
-
-    #need to add ticker logic
-    #need to add check duplicate logic
   end
 end
