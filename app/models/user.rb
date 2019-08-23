894167d82357a7a746ec086c97773da10c428cc2
dillon wyatt <dostaglou@gmail.com>
2019-08-23 13:49:39 +0900

    auto group build done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
+  after_create :hidden_basket_group
+
+  def hidden_basket_group
+    self.groups.build(name: "hidden")
+    self.save
+  end
 end
