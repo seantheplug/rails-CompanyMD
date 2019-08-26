08e77885503980c85f84ff8c4762d46ce984773b
dillon wyatt <dostaglou@gmail.com>
2019-08-26 18:15:56 +0900

    on login / sign in - redirects to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_basket
 
+
   include Pundit
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    root_path
   end
 
   private
