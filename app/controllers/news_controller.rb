ab341a48ac8535727e9fb93e967a6948f2833f1e
dillon wyatt <dostaglou@gmail.com>
2019-08-26 15:07:46 +0900

    moved news into a clickable display that calls api only on click to save time and space

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,10 @@
+class NewsController < ApplicationController
+  skip_after_action :verify_policy_scoped
+  include ApplicationHelper
+
+
+  def index
+    @company = Company.find(params[:company_id])
+    @news_array = company_news(get_company_name(@company.ticker)).sort_by { |h| h[:date] }.reverse
+  end
+end
