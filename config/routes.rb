229f8d6d1c77af1b7daa0dc583e5dafe55946e6e
Geoffrey Wu <gwu20510@gmail.com>
2019-08-26 18:15:07 +0900

    create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'companies#index'
   get 'uikit', to: 'pages#uikit'
   get 'search', to: 'pages#search'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
 
