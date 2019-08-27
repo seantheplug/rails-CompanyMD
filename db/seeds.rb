d844280b6bafb390c4921623df484421d0b4dc00
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-27 12:50:19 +0900

    added NASDAQ ticker back

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,7 @@ end
 # end
 
 MarketIndex.create!(name: "S&P 500 Index", ticker: ".INX")
-# MarketIndex.create!(name: "NASDAQ", ticker: ".IXIC") --> the ticker is not working #Sean
+MarketIndex.create!(name: "NASDAQ", ticker: "NASDAQ:IXIC")
 MarketIndex.create!(name: "DOW JONES", ticker: ".DJI")
 MarketIndex.create!(name: "Russell 2000", ticker: "^RUT")
 puts "created sample"
