85554fda814fbddd31d4800a8247306df951f9a6
Geoffrey Wu <gwu20510@gmail.com>
2019-08-27 17:10:21 +0900

    edit error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   include Pundit
   include MetaTagsHelper
+
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
