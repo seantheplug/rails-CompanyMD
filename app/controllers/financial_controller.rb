53a18c6ae2b6899c8727f3d799c947494ea03f50
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-28 11:35:30 +0900

    added 3 years of data

--- app/controllers/financial_controller.rb
+++ app/controllers/financial_controller.rb
@@ -5,9 +5,22 @@ class FinancialController < ApplicationController
 
   def show
     @company = Company.find(params[:company_id])
-    @financials = financial(@company.ticker, "2018")
-    if @financials.nil?
-      @financials = ""
-    end
+    @financials = financial(@company.ticker)
+    puts "i am here"
+    # year = Time.new.year - 1
+
+    # year_array = []
+    # financials_array
+
+    # i = 0
+
+    # 3.times do
+    #   year_array << year
+    #   year -= 1
+    #   @financials = financial(@company.ticker, year_array[i].to_s)
+    #   if @financials.nil?
+    #     @financials = ""
+    #   end
+    # end
   end
 end
