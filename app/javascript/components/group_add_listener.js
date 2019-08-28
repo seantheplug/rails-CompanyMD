1127e3ea9116ca7816b7e8a6e75c3d390bb658ec
dillon wyatt <dostaglou@gmail.com>
2019-08-28 18:40:31 +0900

    creates a new group

--- app/javascript/components/group_add_listener.js
+++ app/javascript/components/group_add_listener.js
@@ -1,1 +1,16 @@
-console.log("hello")
+var content =
+  `<a href="#">
+    <div class="basket-card">
+      <p class="text-truncate">NEW GROUP</p>
+      <p>0</p>
+      <i class="fas fa-eye"></i>
+    </div>
+  </a>`
+
+var addNewPortfolio = (content) => {
+  console.log("hello oooo");
+  var sideBar = document.querySelector(".basket-cards");
+  sideBar.insertAdjacentHTML('beforeend', content);
+};
+const plusSign = document.getElementById("add-group");
+plusSign.addEventListener("click", (event) => { addNewPortfolio(content) });
