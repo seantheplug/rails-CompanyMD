b593e9dae3091b9acda5557ca808d66c89043490
Kingsley Lau <lau.kingsley@gmail.com>
2019-08-28 14:08:23 +0900

    uncommented out google api calls

--- app/controllers/financial_controller.rb
+++ app/controllers/financial_controller.rb
@@ -6,21 +6,6 @@ class FinancialController < ApplicationController
   def show
     @company = Company.find(params[:company_id])
     @financials = financial(@company.ticker)
-    puts "i am here"
-    # year = Time.new.year - 1
 
-    # year_array = []
-    # financials_array
-
-    # i = 0
-
-    # 3.times do
-    #   year_array << year
-    #   year -= 1
-    #   @financials = financial(@company.ticker, year_array[i].to_s)
-    #   if @financials.nil?
-    #     @financials = ""
-    #   end
-    # end
   end
 end
