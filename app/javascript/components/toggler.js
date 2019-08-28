9eb995d134a117b610d7c4a4d79619a2c12c659a
Geoffrey Wu <gwu20510@gmail.com>
2019-08-28 18:27:56 +0900

    add tabs adn style

--- app/javascript/components/toggler.js
+++ app/javascript/components/toggler.js
@@ -10,18 +10,27 @@ const toggler = () => {
         reportsContent.style.display = "block";
         newsContent.style.display = "none";
         statementContent.style.display = "none";
+        reports.classList.add("active");
+        statement.classList.remove("active");
+        news.classList.remove("active");
     });
 
     statement.addEventListener("click", () => {
         statementContent.style.display = "block";
         reportsContent.style.display = "none";
         newsContent.style.display = "none";
+        reports.classList.remove("active");
+        statement.classList.add("active");
+        news.classList.remove("active");
     });
     
     news.addEventListener("click", () => {
         newsContent.style.display = "block";
         reportsContent.style.display = "none";
         statementContent.style.display = "none";
+        reports.classList.remove("active");
+        statement.classList.remove("active");
+        news.classList.add("active");
     });
 }
 
