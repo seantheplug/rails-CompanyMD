ffdbf1be76ebe7314a8ff433095e7b951cefe3a4
dillon wyatt <dostaglou@gmail.com>
2019-08-29 13:56:39 +0900

    full functionality

--- app/javascript/components/group_add_listener.js
+++ app/javascript/components/group_add_listener.js
@@ -1,14 +1,14 @@
-// The below targets the sidebar and adds an unsaved group with a generic name
+let newPortfolioContent =
 
-var content =
-
-  `<a href="#">
-    <div class="basket-card">
-      <p class="text-truncate">2click to edit</p>
-      <p>0</p>
-      <i class="fas fa-pencil-alt edit nameToChange">
-    </div>
-  </a>`
+  `<div style="display: inline-block">
+    <a href="#">
+      <div class="basket-card">
+        <p class="text-truncate nameToChangeField">New Portfolio</p>
+        <p>0</p>
+      </div>
+    </a>
+    <i class="fas fa-pencil-alt edit nameToAdd" style="background-color: pink">
+  </div>`;
 
 var addNewPortfolio = (content) => {
   var sideBar = document.querySelector(".basket-cards");
@@ -16,10 +16,7 @@ var addNewPortfolio = (content) => {
   callMeBitch()
 };
 const plusSign = document.getElementById("add-group");
-plusSign.addEventListener("click", (event) => { addNewPortfolio(content) });
-
-
-//
+plusSign.addEventListener("click", (event) => { addNewPortfolio(newPortfolioContent) });
 
 var nameContent =
   `<form action="/groups" method="post" id="new_group">
@@ -29,15 +26,33 @@ var nameContent =
 
 
 let callMeBitch = () => {
-  var changeGroupName = (nameContent) => {
-    event.target.innerHTML = nameContent;
+  var addGroupName = (nameContent) => {
+    let nameSpace = event.target.parentNode.querySelector('.nameToChangeField');
+    event.target.parentNode.innerHTML = nameContent;
   };
-  let nameSpace = document.querySelector(".nameToChange");
-  console.log(nameSpace)
-  nameSpace.addEventListener("click", (event) => {changeGroupName(nameContent) });
-}
+  let addPencils = document.querySelectorAll(".nameToAdd");
+  addPencils.forEach((pencil) => {
+    pencil.addEventListener("click", (event) => {addGroupName(nameContent) });
+  });
+};
 
 callMeBitch()
 
+let updateNameContent = (group) => {
+  let name = event.target.parentNode.querySelector('.nameToChangeField');
+  let updatedPortfolioContent =
 
-// submit it with the name= authenticity_token value= 'the value I get from the data'
+  `<form action="/groups/${group}" method="post" id="edit_group_${group}">
+    <input type="hidden" name="authenticity_token" value="${plusSign.dataset.token}">
+    <input type="hidden" name="_method" value="patch">
+    <input type="text" name="group[name]" for="group_name" id="group_name" placeholder="A New Name">
+  </form>`;
+  event.target.parentNode.innerHTML = updatedPortfolioContent;
+};
+
+let changePencils = document.querySelectorAll(".nameToChange");
+changePencils.forEach((pencil) => {
+  var groupValue = pencil.dataset.group
+  pencil.addEventListener("click", (event) => {updateNameContent(groupValue) });
+});
+
